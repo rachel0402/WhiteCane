7e779819a19c1a159aa78434ebd2f9061cd9d96c
shinhyejune <83018010+shinhyejune@users.noreply.github.com>
2024-05-24 01:17:08 +0900

    feat. SoundData+QuizDta
    controller 내용 아직 부족

---
+++ WhiteCane/Assets/02.Scripts/4_Object/QuizOption.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//내가 정답인지 아닌지를 알려주기
+// 내가 눌렸는지 안눌렸는지 알려주기
+
+public class QuizOption : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
